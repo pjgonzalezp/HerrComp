43d9e327ec5210cb835c423ef13ba93b1b044c23
Paula Jessica Gonzalez Prieto <pjgonzalezp@unal.edu.co>
2020-09-04 13:37:27 -0500

    primer intento

---
+++ 2020-09-02-ErrorComputation/prueba.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cdtdlib>
+int main (int argc, char *argv[])
+{
+  std::cout.precision(7);
+  std::cout.setf(std::ios::scientific);
+  
+  double under=1.0, over=1.0;
+  int N=1100
+}
