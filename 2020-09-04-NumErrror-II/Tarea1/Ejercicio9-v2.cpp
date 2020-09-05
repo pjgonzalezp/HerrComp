c02c983d7c5bd6389359574cf525c4d4a603de45
Paula Gonzalez <pjgonzalezp@unal.edu.co>
2020-09-05 12:27:06 -0500

    Ejercicio 9 terminado

--- 2020-09-04-NumErrror-II/Tarea1/Ejercicio9-v2.cpp
+++ 2020-09-04-NumErrror-II/Tarea1/Ejercicio9-v2.cpp
@@ -6,13 +6,14 @@ float fmod(float x);
 
 int main (void)
 {
-    std::cout.precision(8); std::cout.setf(std::ios::scientific);
+   
 
     float x=0.0;
     for(int N=0; N<=100; N++)
     {
+        std::cout.precision(8); std::cout.setf(std::ios::scientific);
         float n=x+(N*0.01);
-        std::cout << x+(N*0.01) << "\t" << f(n) << "\t" <<  fmod(x+(N*0.01)) <<"\t"<< f(n)-fmod(x+(N*0.01))<<"\n";
+        std::cout << x+(N*0.01) << "\t" << f(n) << "\t" <<  fmod(x+(N*0.01)) <<"\n";
     }
 
     return 0;    
@@ -21,7 +22,7 @@ int main (void)
 float f(float x)
 {
     float y;
-    y = 5.0 - sqrt(25+pow(x,2));
+    y = 5.0 - sqrtf(25.0 + x*x);
 
     return y;
 }
@@ -29,7 +30,7 @@ float f(float x)
 float fmod (float x)
 {
     float z;
-    z = -x*x/(5 + sqrt(25 + x*x));
-
+    z = -x*x/(5.0 + sqrtf(25.0 + x*x));
+    
     return z;
 }
